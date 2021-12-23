347b5b8795d2cb491f9487b189147a72794e8947
hbonillac80 <hbonillac@hotmail.com>
2021-12-23 12:30:23 -0500

    Incluyo nueva funcion checksize

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 """ Programa principal - Author: Hugo Bonilla C. - Tarea: Actividad Final """
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, Response
 # Importa módulo que hace el grabado del registro en archivo
 from persistencia import guardar_pedido
 app = Flask (__name__)
@@ -19,3 +19,16 @@ def pizza():
     guardar_pedido(nombre,apellido)
 
     return redirect("http://localhost/solicita_pedido.html", code=302)
+
+@app.route("/checksize",methods=['POST'])
+def checksize():
+    """
+    Comprueba disponibilidad de un tamaño de pizza.
+    """
+    size = request.form.get("p6_list_tampizza")
+    if size=="S":
+        mensaje = "No Disponible"
+    else:
+        mensaje = "Disponible"
+
+    return Response(mensaje, 200, {'Access-Control-Allow-Origin': '*'})
